d7f6a169a129e5186fddb0d67864d9ca1fa11211
omsatpathy <78610886+omsatpathy@users.noreply.github.com>
2021-02-14 15:04:17 +0530

    Update Week-2q3.c

--- Week-2q3.c
+++ Week-2q3.c
@@ -1,9 +1,17 @@
-#include<stdio.h>
-#include<string.h>
-int main()
+#include <stdio.h>
+#include <string.h>
+ 
+void main()
 {
-    char s[50];
-    printf("Enter a string : "); scanf("%s",&s);
-    printf("The total number of words in given string is %d",strlen(s));
-    return 0;
+    char s[200];
+    int count = 0, i;
+ 
+    printf("Enter the string:\n");
+    scanf("%[^\n]s",s);
+    for (i = 0;s[i] != '\0';i++)
+    {
+        if (s[i] == ' ' && s[i+1] != ' ')
+            count++;    
+    }
+    printf("Number of words in given string are: %d\n", count);
 }
